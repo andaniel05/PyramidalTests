ddba8bbd65dff28e96c7fed5bd721406a6fbdb64
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2021-12-22 11:08:37 -0500

    created the UseTraitDecorator

--- src/DSL/PHPUnit/decorators.php
+++ src/DSL/PHPUnit/decorators.php
@@ -1,23 +1,21 @@
 <?php
 
-use ThenLabs\PyramidalTests\DSL\DSL;
-use ThenLabs\PyramidalTests\Model\TestCaseModel;
-use ThenLabs\PyramidalTests\Decorator\AbstractDecorator;
 use ThenLabs\PyramidalTests\Decorator\DecoratorsRegistry;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TestDecorator;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\SetUpDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\EndTestCaseDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\MethodDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\PropertyDecorator;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TearDownDecorator;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TestCaseDecorator;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\EndTestCaseDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\SetUpBeforeClassDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\SetUpBeforeClassOnceDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\SetUpDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\StaticMethodDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\StaticPropertyDecorator;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\SetUpBeforeClassDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TearDownAfterClassDecorator;
-use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\SetUpBeforeClassOnceDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TearDownAfterClassOnceDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TearDownDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TestCaseDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\TestDecorator;
 use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\UseMacroDecorator;
+use ThenLabs\PyramidalTests\DSL\PHPUnit\Decorator\UseTraitDecorator;
 
 DecoratorsRegistry::registerGlobal('testCase', new TestCaseDecorator());
 DecoratorsRegistry::registerGlobal('endTestCase', new EndTestCaseDecorator());
@@ -33,10 +31,4 @@ DecoratorsRegistry::registerGlobal('property', new PropertyDecorator());
 DecoratorsRegistry::registerGlobal('staticMethod', new StaticMethodDecorator());
 DecoratorsRegistry::registerGlobal('method', new MethodDecorator());
 DecoratorsRegistry::registerGlobal('useMacro', new UseMacroDecorator());
-
-DecoratorsRegistry::registerGlobal('useTrait', new class extends AbstractDecorator {
-    public function applyTo(TestCaseModel $testCaseModel, array $arguments)
-    {
-        return DSL::useTrait($arguments[0], $arguments[1] ?? null, $testCaseModel);
-    }
-});
+DecoratorsRegistry::registerGlobal('useTrait', new UseTraitDecorator());
