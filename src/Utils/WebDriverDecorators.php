03154ba9455915f525e004192e610a89d825a8d4
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2021-12-23 15:22:34 -0500

    Created the ClearDecorator

--- src/Utils/WebDriverDecorators.php
+++ src/Utils/WebDriverDecorators.php
@@ -3,11 +3,10 @@ declare(strict_types=1);
 
 namespace ThenLabs\PyramidalTests\Utils;
 
-use Closure;
-use Facebook\WebDriver\WebDriverBy;
 use ThenLabs\PyramidalTests\Decorator\AbstractDecorator;
 use ThenLabs\PyramidalTests\Decorator\PackageInterface;
 use ThenLabs\PyramidalTests\Utils\Decorator\WebDriver\AcceptAlertDecorator;
+use ThenLabs\PyramidalTests\Utils\Decorator\WebDriver\ClearDecorator;
 use ThenLabs\PyramidalTests\Utils\Decorator\WebDriver\ClickDecorator;
 use ThenLabs\PyramidalTests\Utils\Decorator\WebDriver\NavigateDecorator;
 use ThenLabs\PyramidalTests\Utils\Decorator\WebDriver\TypeDecorator;
@@ -23,23 +22,13 @@ class WebDriverDecorators implements PackageInterface
      */
     public static function getDecorators(): array
     {
-        $navigate = new NavigateDecorator();
-        $type = new TypeDecorator();
-        $click = new ClickDecorator();
-        $waitForAlert = new WaitForAlertDecorator();
-        $acceptAlert = new AcceptAlertDecorator();
-
-        $clear = new class extends AbstractDecorator {
-            public function getClosure(array $arguments): ?Closure
-            {
-                $cssSelector = $arguments[0];
-
-                return function () use ($cssSelector) {
-                    static::$driver->findElement(WebDriverBy::cssSelector($cssSelector))->clear();
-                };
-            }
-        };
-
-        return compact('navigate', 'type', 'click', 'waitForAlert', 'acceptAlert', 'clear');
+        return [
+            'navigate'     => new NavigateDecorator(),
+            'type'         => new TypeDecorator(),
+            'click'        => new ClickDecorator(),
+            'waitForAlert' => new WaitForAlertDecorator(),
+            'acceptAlert'  => new AcceptAlertDecorator(),
+            'clear'        => new ClearDecorator(),
+        ];
     }
 }
